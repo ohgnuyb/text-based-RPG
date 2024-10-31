402f4eca3f9827e5c872e1024d7dddd803063bd2
ohgnuyb.b <qudgh5552@naver.com>
2024-10-31 15:15:21 +0900

    index 업데이트 1

--- index.c
+++ index.c
@@ -1,6 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+#include <windows.h>
+enum {
+	BLACK,
+	DARK_BLUE,
+	DARK_GREEN,
+	DARK_SKYBLUE,
+	DARK_RED,
+	DARK_VOILET,
+	DAKR_YELLOW,
+	GRAY,
+	DARK_GRAY,
+	BLUE,
+	GREEN,
+	SKYBLUE,
+	RED,
+	VIOLET,
+	YELLOW,
+	WHITE,
+};
+
+void setColor(unsigned short text) {
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), text);
+}
 
 struct character {
 	char name[20];
@@ -35,6 +58,33 @@ void main() {
 	int choice = -1;
 	int st_ex = -1;
 	int charSel = -1;
+	char title[50] = "Secrets of the Forgotten Kingdom";
+	int totalWidth = 47; // 전체 출력 너비
+	int strWidth = strlen(title); // 문자열 길이
+	int padding = (totalWidth - strWidth) / 2; // 양쪽 패딩 계산
+
+	
+	setColor(RED);
+	printf("     ,      ,\n");
+	printf("      (\\____/)\n");
+	printf("       (_oo_)\n");
+	printf("        (oo)\n");
+	printf("      /------\\/`\n");
+	printf("     / |    ||\n");
+	printf("    *  /\\---/\\\n");
+	printf("       ~~   ~~\n");
+
+	setColor(DARK_VOILET);
+	printf("%*s\n\n", padding + strWidth, title);
+	setColor(DARK_GREEN);
+
+	printf("                               .--\"\"--.\n");
+	printf("                              /        \\\n");
+	printf("                             |   ^  ^  |\n");
+	printf("                             \\  .--.  /\n");
+	printf("                              '.____.'\n");
+	printf("                                ||||\n");
+	setColor(WHITE);
 	printf("1. Start\n2. Exit\n");
 	printf("Enter: ");
 	scanf("%d", &st_ex);
@@ -46,10 +96,27 @@ void main() {
 		printf("Hello! %s\n", playerInfo.playerName);
 		while (1) {
 		printf("------------------------------------------------------------------------------------------\n");
-		printf("Start...\n");
-		printf("Beginning Deep within a vast forest lies the ancient kingdom of Arcadia.Once a prosperous land,\nit fell to ruin under the curse of the dark sorcerer, Maleficent, \nfading from the memories of all who once knew it.\n");
+		setColor(RED);
+		printf("Start!\n");
+		setColor(WHITE);
+		printf("Beginning Deep within a vast forest lies the ancient kingdom of Arcadia. Once a prosperous land,\nit fell to ruin under the curse of the dark sorcerer, Maleficent, \nfading from the memories of all who once knew it.\n");
 		printf("------------------------------------------------------------------------------------------\n");
-		printf("Select character: \n1. Warrior:  A mighty warrior with strong physique and exceptional swordsmanship.\n2. Mage: Mage\n3. Rogue: A cunning rogue with agile movements and expertise in stealth.\n");
+		printf("Select character: ");
+		setColor(GREEN);
+		printf("\n1. Warrior: ");
+		setColor(WHITE);
+		printf("A mighty warrior with strong physique and exceptional swordsmanship.");
+		setColor(BLUE);
+		printf("\n2. Mage: ");
+		setColor(WHITE);
+		printf("A skilled mage wielding powerful magic spells.");
+		setColor(RED);
+		printf("\n3. Rogue: ");
+		setColor(WHITE);
+		printf("A cunning rogue with agile movements and expertise in stealth.\n");
+		setColor(WHITE);
+		
+		
 		printf("Enter: ");
 		scanf("%d", &charSel);
 	
@@ -60,11 +127,15 @@ void main() {
 			playerInfo.attack = 20;
 			playerInfo.defense = 10;
 			printf("------------------------------------------------------------------------------------------\n");
-			printf("You chose Warrior!\n");
+			printf("You chose %s!\n");
+			setColor(RED);
 			printf("  - HP: 120\n");
+			setColor(WHITE);
 			printf("  - Skills: Strong Slash\n");
 			printf("  - Attack: 20\n");
+			setColor(BLUE);
 			printf("  - Defense: 10\n");
+			setColor(WHITE);
 			break;
 			
 		}
@@ -76,10 +147,14 @@ void main() {
 			playerInfo.defense = 0;
 			printf("------------------------------------------------------------------------------------------\n");
 			printf("You chose Mage!\n");
+			setColor(RED);
 			printf("  - HP: 100\n");
+			setColor(WHITE);
 			printf("  - Skills: Fireball\n");
 			printf("  - Attack: 30\n");
+			setColor(BLUE);
 			printf("  - Defense: 0\n");
+			setColor(WHITE);
 			break;
 			
 		}
@@ -91,10 +166,14 @@ void main() {
 			playerInfo.defense = 0;
 			printf("------------------------------------------------------------------------------------------\n");
 			printf("You chose Rogue!\n");
+			setColor(RED);
 			printf("  - HP: 80\n");
+			setColor(WHITE);
 			printf("  - Skills: Evade\n");
 			printf("  - Attack: 40\n");
+			setColor(BLUE);
 			printf("  - Defense: 0\n");
+			setColor(WHITE);
 			break;
 			
 		}
@@ -106,11 +185,12 @@ void main() {
 		}
 		}
 		printf("------------------------------------------------------------------------------------------\n");
-		printf("1. The Path to Arcadia...\n");
+		setColor(RED);
+		printf("The Path to Arcadia...\n");
+		setColor(WHITE);
 		printf("Lost in the dense forest, you come across an ancient stone tablet. \nFaint etchings on the tablet reveal the path to Arcadia.\n");
 		printf("'Through the cursed woods, find the forgotten temple.Defeat the guardian, and open the gates of Arcadia.'\n\n");
 		while (1) {
-
 	
 		printf("Choice: \n");
 		printf("1. Venture into the cursed woods.\n");
@@ -125,9 +205,11 @@ void main() {
 
 		}
 		else {
-
+			printf("------------------------------------------------------------------------------------------\n");
+			printf("Invalid choice. Please select again!\n");
+			while (getchar() != '\n');
 		}
-		} //while exit
+		} 
 
 		
 			
